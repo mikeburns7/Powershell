2597038da1db8f7a1a34c231f5d35cfed5ce7fed
mikeburns7 <57635375+mikeburns7@users.noreply.github.com>
2019-12-04 08:59:54 -0500

    Add Symitar SSH Reg Update

---
+++ symitarssh.ps1
@@ -0,0 +1,59 @@
+<#
+    .SYNOPSIS
+        This script will enable the Symitar Client to use SSH protocol when communicating with the Symitar server 
+    .DESCRIPTION
+        The script will check the client architecture.Tthen if the given registry key exists, the script will modify the registry key if not equal to 1. 
+        Setting the UseSSH registry key value ot 1 will enable SSH for the Symitar client. 
+    .PARAMETERS
+        None
+    .INPUTS
+        None
+    .OUTPUTS
+        None
+    .NOTES
+        Version:        1.0
+        Release Date: 2018-4-18
+        Updated: 2018-4-18
+        Author: Mike Burns
+#>
+
+#check for OS architecture
+if($env:PROCESSOR_ARCHITECTURE -eq 'x86')
+    {
+        #check if registry path exists
+        $pathexists = Test-Path 'HKLM:\SOFTWARE\Symitar\SFW\2.0\Logon Options'
+        if($pathexists -eq $True)
+        {
+            #check if registry key value is not equal to 1
+            if($val.UseSSH -ne 1)
+            {
+                #set registry key value to 1 - enables ssh for symitar client
+                set-itemproperty -Path 'HKLM:\SOFTWARE\Symitar\SFW\2.0\Logon Options' -Name "UseSSH" -Value 1
+            }
+        }
+        else
+        {
+            New-Item -Path "HKLM:\SOFTWARE\Symitar\SFW\2.0\Logon Options" -ItemType Key
+            New-ItemProperty -Path "HKLM:\SOFTWARE\Symitar\SFW\2.0\Logon Options" -Value 1 -PropertyType dword -Name "UseSSH"
+        }
+       
+    
+    }
+#64-bit OS architecture
+else 
+    {
+        $pathexists = Test-Path 'HKLM:\SOFTWARE\Wow6432Node\Symitar\SFW\2.0\Logon Options'
+        if($pathexists -eq $True)
+            {
+                if($val.UseSSH -ne 1)
+                {
+                    Set-itemproperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Symitar\SFW\2.0\Logon Options' -Name "UseSSH" -Value 1
+                }
+            }
+          else  
+           {
+                New-Item -Path 'HKLM:\SOFTWARE\Wow6432Node\Symitar\SFW\2.0\Logon Options' -ItemType Key
+                New-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Symitar\SFW\2.0\Logon Options' -Value 1 -PropertyType dword -Name "UseSSH"
+    }       }
+
+
