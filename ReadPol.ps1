e9adc44c6884b5399dd0a20b4c72683ba2c5a73d
mikeburns7 <57635375+mikeburns7@users.noreply.github.com>
2020-06-09 14:00:15 -0400

    Initial commit
    Reads Group Policy from a endpoint and dumps all .pol files into a readable text file

---
+++ ReadPol.ps1
@@ -0,0 +1,38 @@
+
+ # Requiires https://github.com/PowerShell/GPRegistryPolicy
+ # Install-Module <path>\GPRegistryPolicy
+ # Import-Module GPRegistryPolicy
+
+$files = @(Get-Childitem C:\Windows\System32\GroupPolicy -recurse -filter "*.pol")
+
+$auditpolfile = "C:\users\mike.burns\Desktop\AudtPolOutput.txt"
+New-Item $auditpolfile
+
+foreach ($file in $files) {
+    
+    $polpath = $file.FullName
+    $polsettings = Parse-PolFile -path $polpath
+    $polsettingtext = $polsettings | Out-String  
+    
+    
+    $polpath | Add-Content -Path $auditpolfile  
+    $polsettingtext | Add-Content -Path $auditpolfile
+}
+
+<# 
+$files = @(Get-Childitem C:\Windows\System32\GroupPolicy -recurse -filter "*.inf")
+
+$GptTmplfile = "C:\users\mike.burns\Desktop\GptTmpl.txt"
+New-Item $GptTmplfile
+
+foreach ($file in $files) {
+    
+    $GptTmplpath = $file.FullName
+    copy-item -path $GptTmplpath -destination "C:\users\mike\documents\$file.txt"
+    #$GptTmplsettingstext = Get-Content $polpath
+    #$polsettingtext = $polsettings | Out-String  
+    
+    
+    #$GptTmplpath | Add-Content -Path $GptTmplfile  
+    #$GptTmplsettingtext | Add-Content -Path $GptTmplfile
+}
